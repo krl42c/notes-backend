da18b2dc6529e72886a4a0520de4489d0a658f65
Karol Ciechlecki <karolc9991@gmail.com>
2022-05-14 15:22:56 +0200

    Implemented basic NoteController boilerplate

--- NotesBACKEND/Controllers/NoteController.cs
+++ NotesBACKEND/Controllers/NoteController.cs
@@ -6,9 +6,34 @@ namespace NotesBACKEND.Controllers;
 [Route("/note")]
 public class NoteController
 {
+    private List<Note> noteList = new List<Note>(); //will load notes here through a NoteService
+
+    // This method gets a note from NoteService
     [HttpGet(Name = "GetNote")]
-    public String getNote()
+    public String getNote(int id)
     {
-        return "Testing";
+        //TODO: implement NoteService.getNoteById(id)
+        return null;
     }
+   
+    public List<Note> getAllNotes()
+    {
+        //Returning object for now, will see later if changes are needed
+        return noteList;
+    }
+
+    public void deleteNote(int id)
+    {
+        //TODO: implement NoteService.deleteNoteById(id)
+    }
+
+    public void updateNote(int id, string content = null, string title = null)
+    {
+        //TODO: implement NoteService.updateNoteById(id, ?content, ?title)
+        
+    }
+    
 }
+
+
+
