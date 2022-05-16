f930d0b39dd78a8447df791860c71004fe2bc150
Karol Ciechlecki <karolc9991@gmail.com>
2022-05-16 20:55:41 +0200

    Custom exception boilerplate

---
+++ NotesBACKEND/CustomException.cs
@@ -0,0 +1,19 @@
+﻿namespace NotesBACKEND;
+
+public class CustomException : Exception
+{
+   public CustomException()
+   {
+      
+   }
+
+   public CustomException(string message) : base(message)
+   {
+      
+   }
+
+   public CustomException(string message, Exception inner) : base(message,inner)
+   {
+       
+   }
+}
