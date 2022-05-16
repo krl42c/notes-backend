ea5d5941f5b10ce0dd6b9a54e79c13c05c6177c1
Karol Ciechlecki <karolc9991@gmail.com>
2022-05-16 13:45:16 +0200

    Created custom response object

---
+++ NotesBACKEND/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace NotesBACKEND;
+
+public class ApiResponse<T> where T : Note
+{
+   public ApiResponse(int statusCode, int count, IEnumerable<T> data)
+   {
+      StatusCode = statusCode;
+      Count = count;
+      Data = data;
+   }
+
+   public int StatusCode { get; set; } 
+   public int Count { get; set; }
+   public IEnumerable<T> Data { get; set; }
+
+}
